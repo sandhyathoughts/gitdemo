358739943354a996a82b45a946089b3e3160171c
sandhya <sandhya.vanga@gmail.com>
2021-06-28 14:46:58 +0100

    first commit

---
+++ src/main/java/com/qa/utils/testutil.java
@@ -0,0 +1,9 @@
+package com.qa.utils;
+
+public class testutil {
+
+	public void utiltest() {
+		System.out.println("test utils page");
+		
+	}
+}
---
+++ src/test/java/com/qa/testcases/test1.java
@@ -0,0 +1,9 @@
+package com.qa.testcases;
+
+public class test1 {
+
+	public void test1() {
+		System.out.println("test1");
+		
+	}
+}
